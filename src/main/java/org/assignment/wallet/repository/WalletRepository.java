270c1cb4ea125a791ab448c231b133a9208014ba
serebrianskii <serebrianskiik@gmail.com>
2024-12-02 15:21:36 +0100

    feat: add repos

---
+++ src/main/java/org/assignment/wallet/repository/WalletRepository.java
@@ -0,0 +1,12 @@
+package org.assignment.wallet.repository;
+
+
+import org.assignment.wallet.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+}
