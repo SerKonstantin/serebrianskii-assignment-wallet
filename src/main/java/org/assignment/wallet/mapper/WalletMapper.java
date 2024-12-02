87bcd042d6452ff253dd6895ff7b4e587c1123a5
serebrianskii <serebrianskiik@gmail.com>
2024-12-02 22:47:18 +0100

    fix: mapper

--- src/main/java/org/assignment/wallet/mapper/WalletMapper.java
+++ src/main/java/org/assignment/wallet/mapper/WalletMapper.java
@@ -3,6 +3,7 @@ package org.assignment.wallet.mapper;
 import org.assignment.wallet.dto.WalletBalanceDTO;
 import org.assignment.wallet.model.Wallet;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
@@ -11,5 +12,6 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class WalletMapper {
+    @Mapping(target = "walletId", source = "id")
     public abstract WalletBalanceDTO map(Wallet wallet);
 }
